bc6b3e39d070e18a4babfeddc6482ef0f8730215
Casey Herold <caseynate@gmail.com>
2017-08-23 13:06:08 -0600

    deploying db

--- migrations/20170823102818_author.js
+++ migrations/20170823102818_author.js
@@ -2,8 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('author', (table) => {
   table.increments();
-  table.text('first-name').notNullable();
-  table.text('last-name').notNullable();
+  table.text('first_name').notNullable();
+  table.text('last_name').notNullable();
   table.text('biography').notNullable();
   table.text('portrait').notNullable();
 })
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const knex = require('../db/connection')
+const bodyParser = require('body-parser')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* GET all authors. */
+router.get('/authors', (req, res) => {
+  knex('author')
+  .then((authors) => {
+    res.send(authors)
+  })
+})
 
+router.get('/authors/:id', (req, res) => {
+  let id = req.params.id
+  knex('author')
+  .where('id', id)
+  .then((author) => {
+    res.json(author)
+  })
+
+})
+
+router.get('/books', (req, res) => {
+  knex('book')
+  .then((books) => {
+    res.send(books)
+  })
+})
+
+router.get('/books/:id', (req, res) => {
+  let id = req.params.id
+  knex('book')
+  .where('id', id)
+  .then((book) => {
+    res.send(book)
+  })
+})
 module.exports = router;
